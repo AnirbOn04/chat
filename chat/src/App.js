44a8226fa8926af54d0da5fba9b71c9b4529285d
souvikdas21 <souvikdas.uttarpara@gmail.com>
2024-03-17 13:47:00 +0530

    config next ui

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,21 +1,23 @@
 import LoginForm from './Components/LoginForm/LoginForm';
 import RegisterForm from './Components/RegisterForm/RegisterForm';
 import ForgotPasForm from './Components/ForgotPasForm/ForgotPasForm';
-import {BrowserRouter,Routes,Route} from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {NextUIProvider} from "@nextui-org/react";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/'>
-        <Route index element={<LoginForm />} />
-          <Route path="login" element={<LoginForm />} />
-          <Route path="register" element={<RegisterForm />} />
-          <Route path="forgot" element={<ForgotPasForm />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <NextUIProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/'>
+            <Route index element={<LoginForm />} />
+            <Route path="login" element={<LoginForm />} />
+            <Route path="register" element={<RegisterForm />} />
+            <Route path="forgot" element={<ForgotPasForm />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </NextUIProvider>
   );
 }
 
